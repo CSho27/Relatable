2da32217a149f495b2371be936babad0b8bd322c
Chris Shorter <shorte24@gmail.com>
2024-03-02 15:22:20 -0500

    Adds tests for nullable type conversions and adds extra handling for enums. The handling added for nullable value types is likely unnecessary

--- Relatable/Relatable.UnitTests/Utilities/TypeConversionUtilitiesTests.cs
+++ Relatable/Relatable.UnitTests/Utilities/TypeConversionUtilitiesTests.cs
@@ -7,109 +7,253 @@ namespace Relatable.UnitTests.Utilities
 
   public class TypeConversionUtilitiesTests
   {
+    // ConvertValue
+
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToByte()
+    public void ConvertValue_WhenNullAndTIsValueType_ReturnsDefault()
+    {
+      int? x = null;
+      var result = x.ConvertValue<int>();
+      result.ShouldBe(0);
+    }
+
+    [Fact]
+    public void ConvertValue_WhenNullAndTIsNullableType_ReturnsNull()
+    {
+      int? x = null;
+      var result = x.ConvertValue<int?>();
+      result.ShouldBeNull();
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToEnum()
+    {
+      var x = 0;
+      var result = x.ConvertValue<TestEnum>();
+      result.ShouldBe(TestEnum.A);
+      result.GetType().ShouldBe(typeof(TestEnum));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToNullableEnum()
+    {
+      var x = 0;
+      var result = x.ConvertValue<TestEnum?>();
+      result.ShouldNotBeNull();
+      result.ShouldBe(TestEnum.A);
+      result.GetType().ShouldBe(typeof(TestEnum));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsNullToNullableEnum()
+    {
+      int? x = null;
+      var result = x.ConvertValue<TestEnum?>();
+      result.ShouldBeNull();
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToByte()
     {
       var x = new sbyte();
-      var result = x.ConvertNonNullableValue<byte>();
+      var result = x.ConvertValue<byte>();
       result.GetType().ShouldBe(typeof(byte));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToSByte()
+    public void ConvertValue_ConvertsToNullableByte()
+    {
+      var x = new sbyte();
+      var result = x.ConvertValue<byte?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(byte));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToSByte()
     {
       var x = new byte();
-      var result = x.ConvertNonNullableValue<sbyte>();
+      var result = x.ConvertValue<sbyte>();
       result.GetType().ShouldBe(typeof(sbyte));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToBool()
+    public void ConvertValue_ConvertsToNullableSByte()
+    {
+      var x = new byte();
+      var result = x.ConvertValue<sbyte?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(sbyte));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToBool()
     {
       var x = 1;
-      var result = x.ConvertNonNullableValue<bool>();
+      var result = x.ConvertValue<bool>();
       result.GetType().ShouldBe(typeof(bool));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToShort()
+    public void ConvertValue_ConvertsToNullableBool()
     {
       var x = 1;
-      var result = x.ConvertNonNullableValue<short>();
+      var result = x.ConvertValue<bool?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(bool));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToShort()
+    {
+      var x = 1;
+      var result = x.ConvertValue<short>();
       result.GetType().ShouldBe(typeof(short));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToUShort()
+    public void ConvertValue_ConvertsToNullableShort()
     {
       var x = 1;
-      var result = x.ConvertNonNullableValue<ushort>();
+      var result = x.ConvertValue<short?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(short));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToUShort()
+    {
+      var x = 1;
+      var result = x.ConvertValue<ushort>();
       result.GetType().ShouldBe(typeof(ushort));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToInt()
+    public void ConvertValue_ConvertsToNullableUShort()
+    {
+      var x = 1;
+      var result = x.ConvertValue<ushort?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(ushort));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToInt()
     {
       var x = (long)1;
-      var result = x.ConvertNonNullableValue<int>();
+      var result = x.ConvertValue<int>();
       result.GetType().ShouldBe(typeof(int));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToUInt()
+    public void ConvertValue_ConvertsToNullableInt()
     {
       var x = (long)1;
-      var result = x.ConvertNonNullableValue<uint>();
+      var result = x.ConvertValue<int?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(int));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToUInt()
+    {
+      var x = (long)1;
+      var result = x.ConvertValue<uint>();
       result.GetType().ShouldBe(typeof(uint));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToLong()
+    public void ConvertValue_ConvertsToNullableUInt()
+    {
+      var x = (long)1;
+      var result = x.ConvertValue<uint?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(uint));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToLong()
     {
       var x = 1;
-      var result = x.ConvertNonNullableValue<long>();
+      var result = x.ConvertValue<long>();
       result.GetType().ShouldBe(typeof(long));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToULong()
+    public void ConvertValue_ConvertsToNullableLong()
     {
       var x = 1;
-      var result = x.ConvertNonNullableValue<ulong>();
+      var result = x.ConvertValue<long?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(long));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToULong()
+    {
+      var x = 1;
+      var result = x.ConvertValue<ulong>();
       result.GetType().ShouldBe(typeof(ulong));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToFloat()
+    public void ConvertValue_ConvertsToNullableULong()
+    {
+      var x = 1;
+      var result = x.ConvertValue<ulong?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(ulong));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToFloat()
     {
       var x = 1.1;
-      var result = x.ConvertNonNullableValue<float>();
+      var result = x.ConvertValue<float>();
       result.GetType().ShouldBe(typeof(float));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToDouble()
+    public void ConvertValue_ConvertsToNullableFloat()
     {
       var x = 1;
-      var result = x.ConvertNonNullableValue<double>();
+      var result = x.ConvertValue<float?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(float));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToDouble()
+    {
+      var x = 1;
+      var result = x.ConvertValue<double>();
       result.GetType().ShouldBe(typeof(double));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToDecimal()
+    public void ConvertValue_ConvertsToNullableDouble()
     {
       var x = 1;
-      var result = x.ConvertNonNullableValue<decimal>();
+      var result = x.ConvertValue<double?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(double));
+    }
+
+    [Fact]
+    public void ConvertValue_ConvertsToDecimal()
+    {
+      var x = 1;
+      var result = x.ConvertValue<decimal>();
       result.GetType().ShouldBe(typeof(decimal));
     }
 
     [Fact]
-    public void ConvertNonNullableValue_ConvertsToEnum()
+    public void ConvertValue_ConvertsToNullableDecimal()
     {
-      var x = 0;
-      var result = x.ConvertNonNullableValue<TestEnum>();
-      result.ShouldBe(TestEnum.A);
-      result.GetType().ShouldBe(typeof(TestEnum));
+      var x = 1;
+      var result = x.ConvertValue<decimal?>();
+      result.ShouldNotBeNull();
+      result.GetType().ShouldBe(typeof(decimal));
     }
   }
 }
--- Relatable/Relatable/Utilities/TypeConversionUtilities.cs
+++ Relatable/Relatable/Utilities/TypeConversionUtilities.cs
@@ -1,51 +1,115 @@
-﻿namespace Relatable.Utilities
+﻿
+namespace Relatable.Utilities
 {
   public static class TypeConversionUtilities
   {
     public static T ConvertValue<T>(this object? value)
     {
-      var type = typeof(T);
-      var isTypeNullable = type.IsNullableValueType();
-      if (value is null && !isTypeNullable)
-        throw new ArgumentException("value was null but type T is not a nullable type");
-      if (value is null)
-        return default!;
-      
-      return ConvertNonNullableValue<T>(value);
+
+      return value is null
+        ? default!
+        : (T)ConvertValueType(value, typeof(T))!;
     }
 
-    public static T ConvertNonNullableValue<T>(this object value)
+    private static object? ConvertValueType(object? value, Type type)
     {
-      var type = typeof(T);
-      if (type == typeof(sbyte))
-        return (T)(object)Convert.ToSByte(value);
-      if (type == typeof(byte))
-        return (T)(object)Convert.ToByte(value);
-      if (type == typeof(bool))
-        return (T)(object)Convert.ToBoolean(value);
-      if (type == typeof(short))
-        return (T)(object)Convert.ToInt16(value);
-      if (type == typeof(ushort))
-        return (T)(object)Convert.ToUInt16(value);
-      if (type == typeof(int))
-        return (T)(object)Convert.ToInt32(value);
-      if (type == typeof(uint))
-        return (T)(object)Convert.ToUInt32(value);
-      if (type == typeof(long))
-        return (T)(object)Convert.ToInt64(value);
-      if (type == typeof(ulong))
-        return (T)(object)Convert.ToUInt64(value);
-      if (type == typeof(float))
-        return (T)(object)Convert.ToSingle(value);
-      if (type == typeof(double))
-        return (T)(object)Convert.ToDouble(value);
-      if (type == typeof(decimal))
-        return (T)(object)Convert.ToDecimal(value);
-      if (type.IsEnum)
-        return (T)value;
+      var isNullable = type.IsNullableValueType();
+      var underlyingType = isNullable
+        ? Nullable.GetUnderlyingType(type)!
+        : type;
 
-      throw new ArgumentException($"Unsupported type {type.Name}");
+
+      if(underlyingType.IsEnum)
+      {
+        var values = Enum.GetValues(underlyingType).Cast<object>();
+        return values.Single(v => (int)v == (int)value!);
+      }
+      if (underlyingType == typeof(sbyte))
+      {
+        var convertedValue = Convert.ToSByte(value);
+        return isNullable
+          ? new sbyte?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(byte))
+      {
+        var convertedValue = Convert.ToByte(value);
+        return isNullable
+          ? new byte?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(bool))
+      {
+        var convertedValue = Convert.ToBoolean(value);
+        return isNullable
+          ? new bool?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(short))
+      {
+        var convertedValue = Convert.ToInt16(value);
+        return isNullable
+          ? new short?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(ushort))
+      {
+        var convertedValue = Convert.ToUInt16(value);
+        return isNullable
+          ? new ushort?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(int))
+      {
+        var convertedValue = Convert.ToInt32(value);
+        return isNullable
+          ? new int?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(uint))
+      {
+        var convertedValue = Convert.ToUInt32(value);
+        return isNullable
+          ? new uint?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(long))
+      {
+        var convertedValue = Convert.ToInt64(value);
+        return isNullable
+          ? new long?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(ulong))
+      {
+        var convertedValue = Convert.ToUInt64(value);
+        return isNullable
+          ? new ulong?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(float))
+      {
+        var convertedValue = Convert.ToSingle(value);
+        return isNullable
+          ? new float?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(double))
+      {
+        var convertedValue = Convert.ToDouble(value);
+        return isNullable
+          ? new double?(convertedValue)
+          : convertedValue;
+      }
+      if (underlyingType == typeof(decimal))
+      {
+        var convertedValue = Convert.ToDecimal(value);
+        return isNullable
+          ? new decimal?(convertedValue)
+          : convertedValue;
+      }
+
+      throw new ArgumentException($"Unsupported type {underlyingType.Name}");
     }
-
   }
 }
