c98c24717ce929dac9fabb82c1d0c033177de942
Chris Shorter <shorte24@gmail.com>
2024-03-02 13:59:26 -0500

    Creates initial tests for DbConnectionExtensions

---
+++ Relatable/Relatable.IntegrationTests/Postgres/Execution/DbConnectionExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using Relatable.Execution;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relatable.IntegrationTests.Postgres.Execution
+{
+  public class DbConnectionExtensionTests
+  {
+    [Fact]
+    public void ExecuteScalar_ReturnsInt()
+    {
+      // Arrange
+      using var connection = PostgresConnectionFactory.OpenConnection();
+      var selectInt = "SELECT COUNT(*) FROM test";
+
+      // Act
+      var result = connection.ExecuteScalar<int>(selectInt);
+
+      // Assert
+      result.ShouldBe(1);
+    }
+  }
+}
