9560dea447e9e35165871cc8f1c94e7e587dc48d
Chris Shorter <shorte24@gmail.com>
2024-03-02 09:25:49 -0500

    Creates initial version of DbConnectionExtensions

---
+++ Relatable/Relatable/Execution/DbConnectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace Relatable.Execution
+{
+  public static class DbConnectionExtensions
+  {
+    public static T ExecuteScalar<T>(this IDbConnection connection, string sql) where T : struct
+    {
+      using var command = connection.CreateCommand();
+      command.CommandText = sql;
+      var result = command.ExecuteScalar();
+      return (T)result!;
+    }
+
+    public static async Task<T> ExecuteScalarAsync<T>(this IDbConnection connection, string sql) where T : struct
+    {
+      using var genericCommand = connection.CreateCommand();
+      if (genericCommand is not DbCommand command)
+        throw new InvalidOperationException("Provided DbConnection does not support async execution");
+
+      command.CommandText = sql;
+      var result = await command.ExecuteScalarAsync();
+      return (T)result!;
+    }
+  }
+}
