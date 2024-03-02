d9c64083e1771b2686e86a729b4bafec86a6a503
Chris Shorter <shorte24@gmail.com>
2024-03-02 09:25:19 -0500

    Adds Shouldly to IntegrationTests

--- Relatable/Relatable.IntegrationTests/TestConnection.cs
+++ Relatable/Relatable.IntegrationTests/TestConnection.cs
@@ -1,18 +1,27 @@
+using Shouldly;
 using Npgsql;
+using System.Data;
 
 namespace Relatable.IntegrationTests
 {
   public class IntegrationTest1
   {
     [Fact]
-    public async Task Test1()
+    public void Postgres_CanConnect()
     {
       var connectionString = "Server=localhost:5432;Database=relatable;User Id=postgres;Password=Ba5eba11";
       using var connection = new NpgsqlConnection(connectionString);
       connection.Open();
+      connection.State.ShouldBe(ConnectionState.Open);
+    }
 
+
+    public async Task Method()
+    {
+      var connectionString = "Server=localhost:5432;Database=relatable;User Id=postgres;Password=Ba5eba11";
+      using var connection = new NpgsqlConnection(connectionString);
+      connection.Open();
       var sql = "SELECT * FROM Test";
-
       using var command = new NpgsqlCommand(sql, connection);
       using var reader = command.ExecuteReader();
       while (await reader.ReadAsync())
