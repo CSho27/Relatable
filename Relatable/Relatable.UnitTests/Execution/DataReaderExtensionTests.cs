ba4d39dbc19229aac13a3d460844a042f3ea4baf
Chris Shorter <shorte24@gmail.com>
2024-03-02 22:47:03 -0500

    Beefs up DataReaderExtensionTests and adds testing for populating properties with private, protected, and internal setters

--- Relatable/Relatable.UnitTests/Execution/DataReaderExtensionTests.cs
+++ Relatable/Relatable.UnitTests/Execution/DataReaderExtensionTests.cs
@@ -1,12 +1,7 @@
 ﻿using NSubstitute;
 using Relatable.Execution;
 using Shouldly;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Relatable.UnitTests.Execution
 {
@@ -21,10 +16,19 @@ namespace Relatable.UnitTests.Execution
     public Guid TestGuid { get; set; }
   }
 
+  public class TestVisibility
+  {
+    public bool Public { get; set; }
+    public bool Internal { get; internal set; }
+    public bool Protected { get; protected set; }
+    public bool ProtectedInternal { get; protected internal set; }
+    public bool Private { get; private set; }
+  }
+
   public class DataReaderExtensionTests
   {
     [Fact]
-    public void ConstructRow_CorrectlyConstructsEntity()
+    public void ConstructRow_WithMatchingFieldOrder_CorrectlyConstructsEntity()
     {
       // Arrange
       var dateTime = DateTime.Now;
@@ -36,25 +40,25 @@ namespace Relatable.UnitTests.Execution
       mockDataReader.Read().Returns(true);
       mockDataReader.FieldCount.Returns(7);
       mockDataReader[0].Returns(1);
-      mockDataReader.GetName(0).Returns("Id");
+      mockDataReader.GetName(0).Returns(nameof(TestEntity.Id));
 
       mockDataReader[1].Returns("Test");
-      mockDataReader.GetName(1).Returns("Name");
+      mockDataReader.GetName(1).Returns(nameof(TestEntity.Name));
 
       mockDataReader[2].Returns("Test description");
-      mockDataReader.GetName(2).Returns("Description");
+      mockDataReader.GetName(2).Returns(nameof(TestEntity.Description));
 
       mockDataReader[3].Returns(dateTime);
-      mockDataReader.GetName(3).Returns("CreatedOn");
+      mockDataReader.GetName(3).Returns(nameof(TestEntity.CreatedOn));
 
       mockDataReader[4].Returns(dateOnly);
-      mockDataReader.GetName(4).Returns("CreatedDate");
+      mockDataReader.GetName(4).Returns(nameof(TestEntity.CreatedDate));
 
       mockDataReader[5].Returns(timeOnly);
-      mockDataReader.GetName(5).Returns("CreatedTime");
+      mockDataReader.GetName(5).Returns(nameof(TestEntity.CreatedTime));
 
       mockDataReader[6].Returns(guid);
-      mockDataReader.GetName(6).Returns("TestGuid");
+      mockDataReader.GetName(6).Returns(nameof(TestEntity.TestGuid));
 
       // Act
       var row = mockDataReader.ConstructRow<TestEntity>();
@@ -67,6 +71,87 @@ namespace Relatable.UnitTests.Execution
       row.CreatedDate.ShouldBe(dateOnly);
       row.CreatedTime.ShouldBe(timeOnly);
       row.TestGuid.ShouldBe(guid);
+    }
+
+    [Fact]
+    public void ConstructRow_WithArbitraryFieldOrder_ConstructsEntity()
+    {
+      // Arrange
+      var dateTime = DateTime.Now;
+      var dateOnly = DateOnly.FromDateTime(dateTime);
+      var timeOnly = TimeOnly.FromDateTime(dateTime);
+      var guid = new Guid();
+
+      var mockDataReader = Substitute.For<IDataReader>();
+      mockDataReader.Read().Returns(true);
+      mockDataReader.FieldCount.Returns(7);
+
+      mockDataReader[0].Returns("Test description");
+      mockDataReader.GetName(0).Returns(nameof(TestEntity.Description));
+
+      mockDataReader[1].Returns("Test");
+      mockDataReader.GetName(1).Returns(nameof(TestEntity.Name));
+
+      mockDataReader[2].Returns(1);
+      mockDataReader.GetName(2).Returns(nameof(TestEntity.Id));
+
+      mockDataReader[3].Returns(guid);
+      mockDataReader.GetName(3).Returns(nameof(TestEntity.TestGuid));
+
+      mockDataReader[4].Returns(timeOnly);
+      mockDataReader.GetName(4).Returns(nameof(TestEntity.CreatedTime));
+
+      mockDataReader[5].Returns(dateOnly);
+      mockDataReader.GetName(5).Returns(nameof(TestEntity.CreatedDate));
+
+      mockDataReader[6].Returns(dateTime);
+      mockDataReader.GetName(6).Returns(nameof(TestEntity.CreatedOn));
+
+      // Act
+      var row = mockDataReader.ConstructRow<TestEntity>();
+
+      // Assert
+      row.Id.ShouldBe(1);
+      row.Name.ShouldBe("Test");
+      row.Description.ShouldBe("Test description");
+      row.CreatedOn.ShouldBe(dateTime);
+      row.CreatedDate.ShouldBe(dateOnly);
+      row.CreatedTime.ShouldBe(timeOnly);
+      row.TestGuid.ShouldBe(guid);
+    }
+
+    [Fact]
+    public void ConstructRow_PopulatesAllPublicAndInternalSetters()
+    {
+      // Arrange
+      var mockDataReader = Substitute.For<IDataReader>();
+      mockDataReader.Read().Returns(true);
+      mockDataReader.FieldCount.Returns(5);
+
+      mockDataReader[0].Returns(true);
+      mockDataReader.GetName(0).Returns(nameof(TestVisibility.Public));
+
+      mockDataReader[1].Returns(true);
+      mockDataReader.GetName(1).Returns(nameof(TestVisibility.Internal));
+
+      mockDataReader[2].Returns(true);
+      mockDataReader.GetName(2).Returns(nameof(TestVisibility.Protected));
+
+      mockDataReader[3].Returns(true);
+      mockDataReader.GetName(3).Returns(nameof(TestVisibility.ProtectedInternal));
+
+      mockDataReader[4].Returns(true);
+      mockDataReader.GetName(4).Returns(nameof(TestVisibility.Private));
+
+      // Act
+      var row = mockDataReader.ConstructRow<TestVisibility>();
+
+      // Assert
+      row.Public.ShouldBeTrue();
+      row.Internal.ShouldBeTrue();
+      row.Protected.ShouldBeTrue();
+      row.ProtectedInternal.ShouldBeTrue();
+      row.Private.ShouldBeTrue();
     }
   }
 }
