7938765a466606d0837bc5b993ff0befbf21e77c
Chris Shorter <shorte24@gmail.com>
2024-03-02 23:07:11 -0500

    Adds more rows to table for execute test

--- Relatable/Relatable.IntegrationTests/Postgres/Execution/DbConnectionExtensions/ExecuteTests.cs
+++ Relatable/Relatable.IntegrationTests/Postgres/Execution/DbConnectionExtensions/ExecuteTests.cs
@@ -26,15 +26,19 @@ namespace Relatable.IntegrationTests.Postgres.Execution.DbConnectionExtensions
       // Act
       var results = connection.Execute<TestEntity>(sql);
 
-      results.Count().ShouldBe(1);
-      var onlyResult = results.Single();
-      onlyResult.Id.ShouldBe(1);
-      onlyResult.Name.ShouldBe("TestName");
-      onlyResult.Description.ShouldBe("This is a test description");
-      onlyResult.CreatedOn.ShouldBe(DateTime.Parse("2024-03-02 15:52:56.310556"));
-      onlyResult.CreatedDate.ShouldBe(DateOnly.Parse("2024-03-02"));
-      onlyResult.CreatedTime.ShouldBe(TimeOnly.Parse("16:13:31.333771"));
-      onlyResult.TestUUID.ShouldBe(Guid.Parse("9b6768ba-5140-4b7e-8783-458224a98d06"));
+      results.Count().ShouldBe(3);
+      for(var i=0; i<3; i++)
+      {
+        var result = results.ElementAt(i);
+        result.Id.ShouldBe(i+1);
+        result.Name.ShouldBe("TestName");
+        result.Description.ShouldBe("This is a test description");
+        result.CreatedOn.ShouldBe(DateTime.Parse("2024-03-02 15:52:56.310556"));
+        result.CreatedDate.ShouldBe(DateOnly.Parse("2024-03-02"));
+        result.CreatedTime.ShouldBe(TimeOnly.Parse("16:13:31.333771"));
+        result.TestUUID.ShouldBe(Guid.Parse("9b6768ba-5140-4b7e-8783-458224a98d06"));
+      }
+
     }
   }
 }
